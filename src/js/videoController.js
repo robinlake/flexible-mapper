c212d0b9c042c3427515cbf8e76c35f053ff5de3
robinlake <robinhlake@gmail.com>
2023-04-18 12:29:14 -0700

    css

--- src/js/videoController.js
+++ src/js/videoController.js
@@ -298,7 +298,7 @@ function defineVideoController() {
     var shadow = wrapper.attachShadow({ mode: "open" });
     var shadowTemplate = `
         <style>
-          @import "${chrome.runtime.getURL("/src/css/shadow.css")}";
+          @import "${chrome.runtime.getURL("/css/shadow.css")}";
         </style>
 
         <div id="controller" style="top:${top}; left:${left}; opacity:${
@@ -559,7 +559,7 @@ function initializeNow(document) {
     defineVideoController();
   } else {
     var link = document.createElement("link");
-    link.href = chrome.runtime.getURL("/src/css/inject.css");
+    link.href = chrome.runtime.getURL("/css/inject.css");
     link.type = "text/css";
     link.rel = "stylesheet";
     document.head.appendChild(link);
