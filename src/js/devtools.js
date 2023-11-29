3408917cd8f842a3e701cbfbd67f127da06de5f9
Robin Lake <robin@RobinsMacBookPro.local>
2023-11-29 13:04:19 -0800

    delete unnecessary code

--- src/js/devtools.js
+++
@@ -1,8 +0,0 @@
-chrome.devtools.panels.create('Flexmap', '/images/lips.webp', '/html/panel.html', null);
-
-chrome.devtools.panels.elements.createSidebarPane("Flexmap",
-  function(sidebar) {
-    sidebar.setPage("/html/sidebar.html");
-    sidebar.setHeight("8ex");
-  }
-);
