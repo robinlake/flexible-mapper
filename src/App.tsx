07ded158843dea92f434272dd2500fb978b1a235
Robin Lake <robin@RobinsMacBookPro.local>
2023-11-29 12:44:12 -0800

    move necessary files. Move videospeed code to separate public folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="popup-container flexible-mapper-container">
+        <div className="header">
+          <img src="/images/gender.svg" alt="Flexy" />
+          <h1>Flexmap</h1>
+        </div>
+        <div className="form">
+          <label>Snippets</label>
+          <input
+            type="button"
+            id="youtube_timestamp"
+            value="Get Youtube Timestamp"
+          />
+          <input type="button" id="fetch_foo" value="Fetch Foo" />
+          <input type="button" id="fetch_bar" value="Fetch Bar" />
+        </div>
+        <div className="notifications">
+          <p id="notification"></p>
+        </div>
+        <div className="video-controls-container">
+          <h3>Video controls</h3>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
