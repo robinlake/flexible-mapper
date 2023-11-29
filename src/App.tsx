3de75e25e017cda0c41396c0c570cce672b469fc
Robin Lake <robin@RobinsMacBookPro.local>
2023-11-29 13:09:36 -0800

    first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
 import "./App.scss";
+import Header from "./components/Header.tsx";
 
 function App() {
   return (
-    <>
-      <div className="popup-container flexible-mapper-container">
-        <div className="header">
-          <img src="/images/gender.svg" alt="Flexy" />
-          <h1>Flexmap</h1>
-        </div>
-        <div className="form">
-          <label>Snippets</label>
-          <input
-            type="button"
-            id="youtube_timestamp"
-            value="Get Youtube Timestamp"
-          />
-          <input type="button" id="fetch_foo" value="Fetch Foo" />
-          <input type="button" id="fetch_bar" value="Fetch Bar" />
-        </div>
-        <div className="notifications">
-          <p id="notification"></p>
-        </div>
-        <div className="video-controls-container">
-          <h3>Video controls</h3>
-        </div>
-      </div>
-    </>
+    <div className="popup-container flexible-mapper-container">
+      <Header />
+    </div>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <img src="/images/gender.svg" alt="Flexy" />
+      <h1>Flexmap</h1>
+    </div>
+  );
+};
+export default Header;
