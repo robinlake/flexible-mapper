4c1e7532f48d73330adb819ea41cab702cdb27da
Robin Lake <robin@RobinsMacBookPro.local>
2023-11-30 09:50:11 -0800

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Header from "./components/Header";
+import Upload from "./components/Upload";
 
 function App() {
   return (
     <div className="popup-container">
       <Header />
+      <Upload />
     </div>
   );
 }
